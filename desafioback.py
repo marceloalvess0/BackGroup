35adc913a05abb7adf3a65fc19c90d9ae5e1ea2d
SaraMag25 <saramagalhaes242@gmail.com>
2022-12-12 21:56:51 -0300

    adicionando o encapsulamento dos atributos da Livraria

--- desafioback.py
+++ desafioback.py
@@ -4,6 +4,24 @@ class Livraria():
         self.__nome=nome
         self.__tipo=tipo
         pass
+    @property 
+    def nome(self) :
+        return self.__nome
+    @nome.setter 
+    def nome (self,novo_nome):
+        if type(novo_nome) == str() :
+            self.__nome = novo_nome
+        else:
+            print('Novo nome invalido, digite algo valido')
+    @property
+    def tipo (self) :
+        return self.__tipo
+    @tipo.setter
+    def tipo (self,novo_tipo) :
+        if type(novo_tipo) == str() :
+            self.__tipo = novo_tipo
+        else:
+            print('Novo tipo invalido, digite algo valido')
 class Revista(Livraria):
     def __init__(self, nome,numero_de_paginas,idioma,editora,status):
         super().__init__(nome)
